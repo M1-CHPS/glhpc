53d2fdcbd48549d69d5007e868965b35bcb38fb1
pablooliveira <pablooliveira@users.noreply.github.com>
2025-09-09 12:39:59 +0000

    deploy: bcc0ac970f5dcead1808b2312c808b84f2d11dc0

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -8,10 +8,12 @@ window.MathJax = {
   options: {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
-  }
+  },
+  loader: { load: ['[tex]/color'] },
+  tex: { packages: { '[+]': ['color'] } }
 };
 
-document$.subscribe(() => { 
+document$.subscribe(() => {
   MathJax.startup.output.clearCache()
   MathJax.typesetClear()
   MathJax.texReset()
